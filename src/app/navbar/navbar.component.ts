b32f493a30743fc8989e92de63a8f707c8ac8635
SergeantTobsen <144628669+SergeantTobsen@users.noreply.github.com>
2023-10-31 12:31:22 +0100

    add changes to navbar and footer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  scrollFunction(): void {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      (document.getElementById('navbar') as HTMLElement).style.opacity = '0.8';
+    } else {
+      (document.getElementById('navbar') as HTMLElement).style.opacity = '1';
+    }
+  }
 
+  constructor() {
+    window.onscroll = (): void => {
+      this.scrollFunction();
+    };
+  }
 }
